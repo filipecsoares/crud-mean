1ec98a9849d8d154a229d9294767480f4c2c84ee
Filipe Soares <filipecarlossoares@gmail.com>
2019-07-03 21:46:01 -0300

    Refatoracao da api

--- api/server.js
+++ api/server.js
@@ -1,26 +1,7 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-// const mongoose = require('mongoose');
-const morgan = require('morgan');
-const cors = require('cors');
-// const path = require('path');
-
-const app = express();
-
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
-app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
-app.use(morgan('dev'));
-app.use(cors());
+// SOLID
+const app = require('./src/app');
 
 const port = process.env.PORT || 8000;
-const router = express.Router();
-
-router.get('/', (req, res) => res.json({
-  message: 'Welcome to API Mean',
-}));
-
-app.use('/api', router);
 app.listen(port, () => {
   console.log('Execytando');
 });
