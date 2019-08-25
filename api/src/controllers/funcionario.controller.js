e246e5d32595889a7cf2b1f4afbc39d90acd7e18
Filipe Soares <filipecarlossoares@gmail.com>
2019-08-25 13:12:25 -0300

    Refactoring create method

--- api/src/controllers/funcionario.controller.js
+++ api/src/controllers/funcionario.controller.js
@@ -1,19 +1,9 @@
 const Funcionario = require('../models/funcionario.model');
 
-exports.create = (req, res) => {
-  if (!req.body.nomeFuncionario && !req.body.cargo && !req.body.numeroIdentificador) {
-    return res.status(400).send({ message: 'Os campos não podem estar vazios.' });
-  }
-  const funcionario = new Funcionario({
-    nomeFuncionario: req.body.nomeFuncionario,
-    cargo: req.body.cargo,
-    numeroIdentificador: req.body.numeroIdentificador,
-  });
-  funcionario.save().then((data) => {
-    return res.status(200).send(data);
-  }).catch((err) => {
-    return res.status(500).send({ message: 'Erro ao criar o Funcionário' || err.message });
-  });
+exports.create = async (req, res) => {
+  const novoFuncionario = new Funcionario(req.body);
+  const funcionario = await novoFuncionario.save();
+  res.status(201).send({ message: 'Funcionário cadastrado com sucesso.', funcionario });
 };
 
 exports.findAll = (req, res) => {
--- api/src/routes/funcionario.routes.js
+++ api/src/routes/funcionario.routes.js
@@ -1,6 +1,4 @@
-const express = require('express');
-
-const router = express.Router();
+const router = require('express-promise-router')();
 
 const funcionarioController = require('../controllers/funcionario.controller');
 
